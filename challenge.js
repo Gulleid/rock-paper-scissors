24f1c6827ccf6e77c9c20ea4e8d1613ac709d900
Gulleid <gulleidmohamed93@gmail.com>
2023-05-28 13:02:43 -0800

    Asigment

--- challenge.js
+++ challenge.js
@@ -1,2 +1,18 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+function pG(p1, p2) {
+    if (p1 === p2) {
+      return "ti";
+    } else if (
+      (p1 === "r" && p2 === "s") ||
+      (p1 === "p" && p2 === "rock") ||
+      (p1 === "s" && p2 === "p")
+    ) {
+      return "p1 wins";
+    } else {
+      return "p2 wins";
+    }
+  }
+
+  console.log(pG("r", "p"));
